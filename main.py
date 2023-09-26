0a31c6e7e1b358588ad7eda86649e1463dec117f
ninjagoku4560 <116596186+ninjagoku4560@users.noreply.github.com>
2023-09-26 09:25:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-import requests,time
+import requests,time,function
+  
+commandeDispo = ["help","requests","exit"] 
+commandeDescription =["","send request by using requests library","leave"] 
+  
+print("MIT License")
+print("Copyright (c) 2023 ninjagoku4560") 
+print("NCCP:")
 
-commandeDispo = ["help","requests","exit"]
-commandeDescription =["","send request by using requests library","leave"]
+Close = False
 
-
-commandeEntrer = str(input("•••"))
-
+while not Close:
+    commandeEntrer = str(input("•••"))
+    if function.findCommand(commandeEntrer) == 0:
+        function.help()
+    elif function.findCommand(commandeEntrer) == 1:
+        function.requests()
+    elif function.findCommand(commandeEntrer) == 2:
+        function.exit()
+        break
